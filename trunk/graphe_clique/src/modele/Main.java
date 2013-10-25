fdf72040a8cb4abce263addbea99f1fa2ebb9159
yassine.badih@gmail.com <yassine.badih@gmail.com@3f63861b-085c-83da-9a4f-60956222bf5f>
2013-10-25 12:00:33 +0000

    commit test

--- trunk/graphe_clique/src/modele/Main.java
+++ trunk/graphe_clique/src/modele/Main.java
@@ -4,6 +4,7 @@ public class Main
 {
 	public static void main(String[] args)
 	{
+		
 		Graphe graphe = new Graphe("fichier.txt");
 		Clique clique = new Clique(graphe);
 		clique.rechercheClique();
